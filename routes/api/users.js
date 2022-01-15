741431958571a1a07c9216a2278626e7fa97f8ba
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2022-01-15 13:41:14 +0200

    step 4 and connect cloudinary

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,17 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
-const multer = require('multer');
+// const path = require('path');
+// const Jimp = require('jimp');
+var cloudinary = require('cloudinary').v2;
 
 const { SECRET_KEY } = process.env;
 
+// const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
+
 const { joiRegisterSchema, joiLoginSchema } = require('../../model/user');
 const { User } = require('../../model');
-const { authentication } = require('../../middlewares');
+const { authentication, upload } = require('../../middlewares');
 
 const router = express.Router();
 
@@ -102,12 +106,50 @@ router.get('/current', authentication, async (req, res) => {
   res.json({ user: { email, subscription } });
 });
 
-router.patch('/avatars', authentication, async (req, res, next) => {
-  try {
-    //
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  '/avatars',
+  authentication,
+  upload.single('avatar'),
+  async (req, res, next) => {
+    try {
+      const { path: tempUpload } = req.file;
+
+      // Jimp.read(tempUpload, (err, image) => {
+      //   if (err) throw err;
+      //   image
+      //     .resize(250, 250) // resize
+      //     .write(tempUpload); // save
+      // });
+
+      let tempAvatarURL = '';
+
+      await cloudinary.uploader.upload(
+        tempUpload,
+        {
+          transformation: [
+            { width: 250, height: 250, gravity: 'face', crop: 'thumb' },
+          ],
+        },
+        function (error, result) {
+          if (error) {
+            next(error);
+          }
+
+          tempAvatarURL = result.url;
+        },
+      );
+
+      await User.findByIdAndUpdate(
+        req.user._id,
+        { avatarURL: tempAvatarURL },
+        { new: true },
+      );
+
+      res.json({ avatarURL: tempAvatarURL });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 module.exports = router;
